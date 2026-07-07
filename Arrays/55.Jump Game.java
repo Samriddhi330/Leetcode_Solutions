829549810584ee3a643f13e1e5e712d20011f46a
Samriddhi330 <srivastavasamriddhi33@gmail.com>
2026-07-07 20:58:37 +0530

    Implement canJump method to check jump feasibility

---
+++ Arrays/55.Jump Game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(i<=farthest){
+                farthest = Math.max(farthest,i + nums[i]);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
